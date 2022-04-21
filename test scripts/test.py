65bf858f8ad9160600eafef2b9b14312a9a4d69a
Janis <76683226+saschazesiger@users.noreply.github.com>
2022-04-21 18:18:32 +0200

    Update URL List

--- test scripts/test.py
+++ test scripts/test.py
@@ -5,13 +5,19 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-rawurl = "https://premproxy.com/list/0[page].htm"
 headers = {'User-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'}
+proxylist = []
+sourcefile = open("C:/Proxy-List/Sources.txt", "r")
+sourcelist = sourcefile.readlines()
+
+
+
 
 
 #Gets HTML from specific URL
 def gethtml(rawurl, headers):
     proxies = ["leer"]
+    proxylist = []
     page = 0
     if "[page]" in rawurl:
 #If Site has more than one pages, it will run multiple times
@@ -21,34 +27,47 @@ def gethtml(rawurl, headers):
             else:
                 page = page + 1
             url = rawurl.replace("[page]",f"{page}")
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers, timeout=10)
             soup = BeautifulSoup(response.content, 'html.parser')
-
-            proxies = extract2(soup)
-            print(len(proxies), page)
+            proxies =  extract(soup)
+            proxylist = proxylist + proxies
+            print(len(proxies), len(proxylist), url)
+            
     else:
-        response = requests.get(rawurl, headers=headers)
+        response = requests.get(rawurl, headers=headers, timeout=10)
         soup = BeautifulSoup(response.content, 'html.parser')
-        
+        proxylist = extract(soup)
+    return(proxylist)
 
-        proxies = extract2(soup)
-        print(len(proxies))
-
-#Extracts IP:PORT from HTML
-def extract1(soup):
-    text = f"{soup.body}"
-    proxies = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b", text)
-    return proxies
 
 #Extracts IP:PORT from HTML but they don't need to be in the same tag
-def extract2(soup):
+def extract(soup):
     text = soup.get_text(separator=":")
-    text = text.replace("\n",":").replace("::",":").replace("::",":")
+    text = text.replace("\n",":").replace(" ", "")
+    while "::" in text:
+        text = text.replace("::", ":")
     #print(text)
     proxies = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b", text)
+    if len(proxies) < 1:
+        text = f"{soup.body}"
+        proxies = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b", text)
+    #for proxy in proxies:
+        #print(proxy)
+    
     return proxies
+
 
 
 
 #TEST
-gethtml(rawurl, headers)
+#for source in sourcelist:
+    rawurl = source.replace("\n", "")
+    if "://" in rawurl:
+        proxies = gethtml(rawurl, headers) 
+        proxylist = proxylist + proxies
+        print(len(proxies),len(proxylist), rawurl)
+    else:
+        print(source)
+
+proxies = gethtml("https://proxyservers.pro/proxy/list/order/updated/order_dir/desc/page/1", headers)
+print(proxies)
